979b32aaedf1fd6254b191afa65aceb34380bf60
svopex <svopex@gmail.com>
2024-02-20 10:45:59 +0100

    Revert "Anti martingale."
    This reverts commit 4f70299ab6eaa1ee544a40c556d0a68647f8af50.

--- SvpTradingPanel/FormTradingPanel.cs
+++ SvpTradingPanel/FormTradingPanel.cs
@@ -163,17 +163,15 @@ namespace SvpTradingPanel
 			{
 				if (checkBoxPendingOrder.Checked)
 				{
-					// 52-39-9 kvuli martingale.
-					MetatraderInstance.Instance.CreatePendingOrderSlPtPercent(price.Value, CalculatePosition(positionSize.Value, 0.52), 1, GetTpDistanceByOrderSize(52));
-					MetatraderInstance.Instance.CreatePendingOrderSlPtPercent(price.Value, CalculatePosition(positionSize.Value, 0.39), 1, GetTpDistanceByOrderSize(39));
-					MetatraderInstance.Instance.CreatePendingOrderSlPtPercent(price.Value, CalculatePosition(positionSize.Value, 0.09), 1, GetTpDistanceByOrderSize(9));
+					MetatraderInstance.Instance.CreatePendingOrderSlPtPercent(price.Value, CalculatePosition(positionSize.Value, 0.5), 1, GetTpDistanceByOrderSize(50));
+					MetatraderInstance.Instance.CreatePendingOrderSlPtPercent(price.Value, CalculatePosition(positionSize.Value, 0.4), 1, GetTpDistanceByOrderSize(40));
+					MetatraderInstance.Instance.CreatePendingOrderSlPtPercent(price.Value, CalculatePosition(positionSize.Value, 0.1), 1, GetTpDistanceByOrderSize(10));
 				}
 				else
 				{
-					// 52-39-9 kvuli martingale.
-					MetatraderInstance.Instance.CreateMarketOrderSlPtPercent(CalculatePosition(positionSize.Value, 0.52), 1, GetTpDistanceByOrderSize(52));
-					MetatraderInstance.Instance.CreateMarketOrderSlPtPercent(CalculatePosition(positionSize.Value, 0.39), 1, GetTpDistanceByOrderSize(39));
-					MetatraderInstance.Instance.CreateMarketOrderSlPtPercent(CalculatePosition(positionSize.Value, 0.09), 1, GetTpDistanceByOrderSize(9));
+					MetatraderInstance.Instance.CreateMarketOrderSlPtPercent(CalculatePosition(positionSize.Value, 0.5), 1, GetTpDistanceByOrderSize(50));
+					MetatraderInstance.Instance.CreateMarketOrderSlPtPercent(CalculatePosition(positionSize.Value, 0.4), 1, GetTpDistanceByOrderSize(40));
+					MetatraderInstance.Instance.CreateMarketOrderSlPtPercent(CalculatePosition(positionSize.Value, 0.1), 1, GetTpDistanceByOrderSize(10));
 				}
 				JoinSl();
 			}
